6710cc391160a845d0899902ecec9a1dc08dd2c2
Intars Voitkevičs <intars.voitkevics@sk.lvt.lv>
2026-01-26 12:06:52 +0200

    Realizēta vārda ievade

---
+++ Assets/Scripts/NameScript.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameScript : MonoBehaviour
+{
+    private string text;
+    private string[] input = {"Sveiks", "Jauku dienu", "Prieks Tevi redzēts", "Uz redzēšanos", "Jauki, ka atnāci", "Tiksimies rīt!"};
+    private int rand;
+    public GameObject inputField;
+    public GameObject textField;
+
+    public void GetText()
+    {
+        rand = Random.Range(0, input.Length);
+        text = inputField.GetComponent<TMP_InputField>().text;
+        textField.GetComponent<TMP_Text>().text = input[rand] + " " + text + "!"; 
+    }
+}
